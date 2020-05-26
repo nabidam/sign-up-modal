8a3e1b6c9813bc7419a56cce391055b7a950813c
Navid <navidabbaspoor@gmail.com>
2020-05-26 22:03:24 +0430

    add tabs to modal

--- scripts.js
+++ scripts.js
@@ -8,4 +8,28 @@ jQuery(document).ready(function ($) {
       $(".modal-container").hide();
     }
   });
+
+  $(".modal-tab").on("click", function () {
+    // console.log(this);
+    var this_id = $(this).children("a").attr("id");
+    switch (this_id) {
+      case "login-tab":
+        $("#login-tab").parent().addClass("is-selected");
+        $("#signup-tab").parent().removeClass("is-selected");
+
+        $("#login-context").addClass("is-selected");
+        $("#signup-context").removeClass("is-selected");
+        break;
+      case "signup-tab":
+        $("#login-tab").parent().removeClass("is-selected");
+        $("#signup-tab").parent().addClass("is-selected");
+
+        $("#login-context").removeClass("is-selected");
+        $("#signup-context").addClass("is-selected");
+        break;
+      default:
+        break;
+    }
+    console.log(this_id);
+  });
 });
